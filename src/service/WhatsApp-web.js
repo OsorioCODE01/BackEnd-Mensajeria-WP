20edf72270e294c8ed5fb6fc23d12fb9a7883c01
OsorioCODE01 <nicolas.osor.gal@gmail.com>
2024-10-06 08:41:30 -0500

    No cambios que no habia subido

---
+++ src/service/WhatsApp-web.js
@@ -0,0 +1,33 @@
+const { Client , LocalAuth} = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client({
+    webVersionCache: {
+      type: "remote",
+      remotePath:
+        "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+    },
+    authStrategy: new LocalAuth(),
+  });
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('message_create', message => {
+	if (message.body === '!ping') {
+		// reply back "pong" directly to the message
+		message.reply('pong');
+	}
+});
+
+
+client.on('error', err => {
+    console.error('Error:', err);
+});
+
+
+client.initialize();
